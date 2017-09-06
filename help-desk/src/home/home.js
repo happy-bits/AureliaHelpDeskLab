c57bd85435a9615dab41818babea78072de1a071
Oscar Olsson <happybits@gmail.com>
2017-09-06 16:27:56 +0200

    i) Continuing.. home.js needs to be updated as well to get the activities

--- help-desk/src/home/home.js
+++ help-desk/src/home/home.js
@@ -1,3 +1,16 @@
+import { inject } from 'aurelia-framework';
+import { Server } from 'backend/server';
+
+@inject(Server)
 export class Home {
-  
+    constructor(server) {
+        this.server = server;
+        this.activity = null;
+    }
+
+    activate() {
+        return Promise.all([
+            this.server.getRecentActivity().then(activity => this.activity = activity),
+        ]);
+    }
 }
